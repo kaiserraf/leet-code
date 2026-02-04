c32dbea24b7185095828443dc74de38c935935ec
kaiserraf <rafaelmachado2507oliveira@hotmail.com>
2026-02-04 15:16:45 -0300

    firts commit + twosum

---
+++ README.md
@@ -0,0 +1,9 @@
+# LeetCode Solutions 🚀
+
+Repositório destinado ao armazenamento e organização de resoluções de problemas da plataforma *LeetCode*. O foco é o desenvolvimento de raciocínio lógico e o estudo de estruturas de dados através de múltiplas linguagens de programação.
+
+## 🛠️ Linguagens e Tecnologias
+Este repositório é multilinguagem, focado em praticar a sintaxe e as particularidades de cada linguagem
+
+## 🧠 Objetivo
+Documentar minha evolução em algoritmos e manter um histórico prático de estudos durante a graduação de Engenharia de Software.
---
+++ TwoSum.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dic = new Dictionary<int, int>(); // hash map para adicionar valores e comparar
+
+        int i = 0;
+        
+        for(i = 0; i < nums.Length; i++) // loop para ler todo o array e poder comparar os valores
+        {
+            var comp = target - nums[i]; // cria um valor de comparação
+
+            if (!dic.ContainsKey(comp)) // se o valor não está no hash map -> adiciona
+            {
+                dic.TryAdd(nums[i], i); // adiciona valor com TryAdd -- para não travar a execução
+            }
+            else
+            {
+                return new int[] {dic[comp], i}; // se estiver no hash map, encontrou a solução
+            }
+        }
+
+        return new int[0]; // retorno para o c# nn encher o saco
+    }
+}
